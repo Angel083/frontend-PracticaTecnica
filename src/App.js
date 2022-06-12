c212300b986bff1ef4de522a8f4220655c6a3d05
AngelGM <cesargasca083@gmail.com>
2022-06-11 20:48:45 -0500

    Rutas agregadas, movimiento entre paginas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Login } from './pages/Login';
+import { Routes, Route } from "react-router-dom";
+import { Home } from './pages/Home';
+import { AddProduct } from './pages/AddProduct';
+import EditProduct from './pages/EditProduct';
+import { GetProduct } from './pages/GetProduct';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/addProduct' element={<AddProduct/>}></Route>
+      <Route path='/editProduct' element={<EditProduct/>}></Route>
+      <Route path='/getProduct' element={<GetProduct/>}></Route>
+    </Routes>
   );
 }
 
